19b930f62bfa5689f3d4b3784d7603daa3b9e558
David <pilecekd@gmail.com>
2026-03-19 21:32:05 +0100

    test

--- t1.m
+++ t1.m
@@ -1,1 +1,14 @@
 
+A1 = load("1_input.mat")
+P1 = load("1_pendulum.mat")
+I2 = load("2_input.mat")
+I7 = load("7_input.mat")
+P7 = load("7_pendulum.mat")
+
+I6 = load("6_input.mat")
+P6 = load("6_pendulum.mat")
+
+plot(I6.ans)
+grid on
+hold on
+plot(P6.ans)
